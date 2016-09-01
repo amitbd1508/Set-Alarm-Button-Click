4f5ed92a15517d6275df608256e8ffab6867c63b
rafid asrar <rafidasrar@gmail.com>
2016-09-02 01:09:08 +0600

    Initial commit

---
+++ app/src/main/java/com/blackflag/com/set_alarm_button_click/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package com.blackflag.com.set_alarm_button_click;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by me on 02/09/16.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context k1, Intent k2) {
+        // TODO Auto-generated method stub
+        Toast.makeText(k1, "Sonet Alarm is started it is not defult alarm youcan add your code here!", Toast.LENGTH_LONG).show();
+
+    }
+
+}
